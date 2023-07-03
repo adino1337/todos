efd7c6873cba864b9cd9bffb1757bc11c1074910
Andrej Tejbus <andrejado40@gmail.com>
2023-07-03 15:00:01 +0200

    working on task 7

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,9 +2,27 @@ import React from 'react';
 
 // 4. Vytvor samostatny modul pre komponent TodoList a nasledne ho importuj v App.js.
 class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: "all",
+    };
+  }
+
+  handleMode = e => {
+    this.setState({mode: e.value});
+  }
+
     render() {
       return (
         <ul>
+          <span>All</span>
+          <input type="radio" name="mode" {...this.state.mode==="all" && "checked"} value="all" onChange={this.handleMode} /><br/>
+          <span>ACTIVE</span>
+          <input type="radio" name="mode" value="active" onChange={this.handleMode} /><br/>
+          <span>DONE</span>
+          <input type="radio" name="mode" value="done" onChange={this.handleMode} /><br/>
+          <h2>{this.state.mode}</h2>
           {this.props.items.map(item => (
             <li className={item.deleted && "delete"} key={item.id} onClick={(e) => this.props.onClick(item)}>{item.text}</li>
           ))}
