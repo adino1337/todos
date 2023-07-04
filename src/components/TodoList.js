fbf00a0673171d3bb179f84cd1f1da68c73ea958
Andrej Tejbus <andrejado40@gmail.com>
2023-07-04 08:35:41 +0200

    tasks 1,2,3,4,5,6,7

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,23 +7,31 @@ class TodoList extends React.Component {
     this.state = {
       mode: "all",
     };
+    this.props = props;
+    this.handleMode.bind(this);
   }
 
   handleMode = e => {
-    this.setState({mode: e.value});
-  }
+    this.setState({mode: e.target.value});     
+  }  
 
-    render() {
+    render() {   
       return (
         <ul>
           <span>All</span>
-          <input type="radio" name="mode" {...this.state.mode==="all" && "checked"} value="all" onChange={this.handleMode} /><br/>
+          <input type="radio" name="mode" checked={this.state.mode==="all"} value="all" onChange={this.handleMode} /><br/>
           <span>ACTIVE</span>
-          <input type="radio" name="mode" value="active" onChange={this.handleMode} /><br/>
+          <input type="radio" name="mode" checked={this.state.mode==="active"} value="active" onChange={this.handleMode} /><br/>
           <span>DONE</span>
-          <input type="radio" name="mode" value="done" onChange={this.handleMode} /><br/>
-          <h2>{this.state.mode}</h2>
-          {this.props.items.map(item => (
+          <input type="radio" name="mode" checked={this.state.mode==="done"} value="done" onChange={this.handleMode} /><br/>
+          {this.props.items.filter((item) => {
+            if(this.state.mode==="all")
+              return true 
+            if(this.state.mode == "active")
+              return !item.deleted
+            if(this.state.mode == "done")
+              return item.deleted           
+          }).map(item => (
             <li className={item.deleted && "delete"} key={item.id} onClick={(e) => this.props.onClick(item)}>{item.text}</li>
           ))}
         </ul>
