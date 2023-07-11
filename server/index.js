56c6ca79207bb26fb6b44e7d6622050a87715991
Andrej Tejbus <andrejado40@gmail.com>
2023-07-11 12:24:00 +0200

    class based done

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const connectDB = require('./db/connect');
 var cors = require('cors')
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const todosRouter = require('./routes/todos')
 
 // middleware
